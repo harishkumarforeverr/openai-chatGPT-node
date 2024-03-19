358022a2907827dd3525c7503409738f1e1e4ccd
harishkumarforever <harishking809@gmail.com>
2024-03-19 10:33:37 +0530

    ghvvjhkjlk

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import OpenAI from "openai";
+import { useEffect } from "react";
+
+// const openai = new OpenAI();
+
+// async function main() {
+//   const completion = await openai.chat.completions.create({
+//     messages: [{ role: "system", content: "You are a helpful assistant." }],
+//     model: "gpt-3.5-turbo",
+//   });
+
+//   console.log(completion.choices[0]);
+// }
 
 function App() {
+  useEffect(() => {
+    // main();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
